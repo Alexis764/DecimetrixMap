37397b0973e11e7f14e2e27ba57c7ab607309a1c
Alexis764 <arevaloalexis764@gmail.com>
2025-04-28 06:55:42 -0500

    Dependency Injection Configuration

--- app/src/main/java/com/alexisarevalor/decimetrixmap/MainActivity.kt
+++ app/src/main/java/com/alexisarevalor/decimetrixmap/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.alexisarevalor.decimetrixmap.ui.theme.DecimetrixMapTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
