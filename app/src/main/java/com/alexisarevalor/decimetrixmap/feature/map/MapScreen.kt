3048eb5f8b2dea764cbac00085fa01df1f0e6b7d
Alexis764 <arevaloalexis764@gmail.com>
2025-04-29 17:11:07 -0500

    Loading the map and user location

--- app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapScreen.kt
+++ app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapScreen.kt
@@ -1,11 +1,37 @@
 package com.alexisarevalor.decimetrixmap.feature.map
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.mapbox.maps.extension.compose.MapEffect
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
+import com.mapbox.maps.plugin.locationcomponent.location
 
 @Composable
 fun MapScreen(
     mapViewModel: MapViewModel = hiltViewModel()
 ) {
+    val mapViewportState = rememberMapViewportState()
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+            modifier = Modifier.fillMaxSize(),
+            mapViewportState = mapViewportState
+        ) {
+            MapEffect(Unit) { mapView ->
+                mapView.location.updateSettings {
+                    locationPuck = createDefault2DPuck(withBearing = true)
+                    enabled = true
+                    puckBearing = PuckBearing.COURSE
+                    puckBearingEnabled = true
+                }
+                mapViewportState.transitionToFollowPuckState()
+            }
+        }
+    }
 }
