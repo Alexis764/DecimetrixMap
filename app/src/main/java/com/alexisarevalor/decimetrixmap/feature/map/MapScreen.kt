f796bfdec02e1699972639540923303679118550
Alexis764 <arevaloalexis764@gmail.com>
2025-04-30 02:07:18 -0500

    Load GeoJson as pointers on the map

--- app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapScreen.kt
+++ app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapScreen.kt
@@ -1,29 +1,64 @@
 package com.alexisarevalor.decimetrixmap.feature.map
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.alexisarevalor.decimetrixmap.R
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraBoundsOptions
 import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.extension.compose.annotation.generated.PointAnnotation
+import com.mapbox.maps.extension.compose.annotation.rememberIconImage
+import com.mapbox.maps.extension.compose.rememberMapState
 import com.mapbox.maps.plugin.PuckBearing
 import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
 import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.maps.toCameraOptions
+import kotlinx.coroutines.launch
 
 @Composable
 fun MapScreen(
     mapViewModel: MapViewModel = hiltViewModel()
 ) {
+    //Screen states
+    val currentPointList = mapViewModel.currentPointList
+    val context = LocalContext.current
+
+    //Map States
     val mapViewportState = rememberMapViewportState()
 
+    val coroutineScope = rememberCoroutineScope()
+    var cameraChanged by rememberSaveable { mutableStateOf(false) }
+    val mapState = rememberMapState {
+        coroutineScope.launch {
+            cameraChangedEvents.collect { _ ->
+                cameraChanged = !cameraChanged
+            }
+        }
+    }
+
+    //Map content
     Box(modifier = Modifier.fillMaxSize()) {
         MapboxMap(
             modifier = Modifier.fillMaxSize(),
-            mapViewportState = mapViewportState
+            mapViewportState = mapViewportState,
+            mapState = mapState
         ) {
+            // Map setup
             MapEffect(Unit) { mapView ->
+                // Set the map's camera to the user's location
                 mapView.location.updateSettings {
                     locationPuck = createDefault2DPuck(withBearing = true)
                     enabled = true
@@ -31,6 +66,43 @@ fun MapScreen(
                     puckBearingEnabled = true
                 }
                 mapViewportState.transitionToFollowPuckState()
+
+                // Set the map's min zoom
+                val cameraBoundsOptions = CameraBoundsOptions
+                    .Builder()
+                    .minZoom(7.0)
+                    .build()
+
+                mapView.mapboxMap.setBounds(cameraBoundsOptions)
+            }
+
+            // Do something when the camera changes
+            MapEffect(cameraChanged) { mapView ->
+                val cameraBounds = mapView
+                    .mapboxMap
+                    .coordinateBoundsForCamera(mapView.mapboxMap.cameraState.toCameraOptions())
+
+                mapViewModel.setCurrentPointsList(cameraBounds)
+            }
+
+            // Draw the points on the map
+            val marker = rememberIconImage(
+                key = R.drawable.red_marker,
+                painter = painterResource(R.drawable.red_marker)
+            )
+            currentPointList.forEach { place ->
+                PointAnnotation(
+                    point = Point.fromLngLat(
+                        /* longitude = */ place.geometry.coordinates[0],
+                        /* latitude = */ place.geometry.coordinates[1]
+                    )
+                ) {
+                    iconImage = marker
+                    interactionsState.onClicked {
+                        Toast.makeText(context, place.properties.name, Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapViewModel.kt
+++ app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapViewModel.kt
@@ -1,10 +1,13 @@
 package com.alexisarevalor.decimetrixmap.feature.map
 
-import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alexisarevalor.decimetrixmap.feature.map.data.Feature
 import com.alexisarevalor.decimetrixmap.feature.map.data.PlacesService
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CoordinateBounds
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,13 +17,31 @@ class MapViewModel @Inject constructor(
     private val placesService: PlacesService
 ) : ViewModel() {
 
+    private val placesList = mutableListOf<Feature>()
+
     init {
-        CoroutineScope(Dispatchers.IO).launch {
+        viewModelScope.launch(Dispatchers.IO) {
             val placesResponse = placesService.getPlaces()
             if (placesResponse.isSuccessful && placesResponse.body() != null) {
-                Log.e("Places", "Places: ${placesResponse.body()!!.features}")
+                placesList.addAll(placesResponse.body()!!.features)
             }
         }
+    }
+
+
+    private val _currentPointsList = mutableStateListOf<Feature>()
+    val currentPointList: List<Feature> = _currentPointsList
+
+    fun setCurrentPointsList(cameraBounds: CoordinateBounds) {
+        val points = placesList.filter { place ->
+            val longitude = place.geometry.coordinates[0]
+            val latitude = place.geometry.coordinates[1]
+            val point = Point.fromLngLat(longitude, latitude)
+            cameraBounds.contains(point, true)
+        }
+
+        _currentPointsList.clear()
+        _currentPointsList.addAll(points)
     }
 
 }
