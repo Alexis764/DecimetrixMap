760ceee7aa1f7b75642431524831e79a5604bb50
Alexis764 <arevaloalexis764@gmail.com>
2025-04-30 22:18:34 -0500

    Network status observer

--- app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapScreen.kt
+++ app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapScreen.kt
@@ -4,55 +4,44 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.mapbox.geojson.Point
-import com.mapbox.maps.CameraBoundsOptions
 import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
-import com.mapbox.maps.extension.compose.annotation.generated.CircleAnnotation
-import com.mapbox.maps.extension.compose.rememberMapState
 import com.mapbox.maps.plugin.PuckBearing
 import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
 import com.mapbox.maps.plugin.locationcomponent.location
-import com.mapbox.maps.toCameraOptions
-import kotlinx.coroutines.launch
 
 @Composable
 fun MapScreen(
     mapViewModel: MapViewModel = hiltViewModel()
 ) {
     //Screen states
-    val currentPointList = mapViewModel.currentPointList
     val context = LocalContext.current
+    val isConnected by mapViewModel.networkStatus.collectAsState()
+
+    LaunchedEffect(isConnected) {
+        if (!isConnected) {
+            Toast.makeText(context, "No internet connection", Toast.LENGTH_SHORT).show()
+
+        } else {
+            mapViewModel.getPlaces()
+        }
+    }
 
     //Map States
     val mapViewportState = rememberMapViewportState()
-
-    val coroutineScope = rememberCoroutineScope()
-    var cameraChanged by rememberSaveable { mutableStateOf(false) }
-    val mapState = rememberMapState {
-        coroutineScope.launch {
-            cameraChangedEvents.collect { _ ->
-                cameraChanged = !cameraChanged
-            }
-        }
-    }
 
     //Map content
     Box(modifier = Modifier.fillMaxSize()) {
         MapboxMap(
             modifier = Modifier.fillMaxSize(),
-            mapViewportState = mapViewportState,
-            mapState = mapState
+            mapViewportState = mapViewportState
         ) {
             // Map setup
             MapEffect(Unit) { mapView ->
@@ -64,46 +53,55 @@ fun MapScreen(
                     puckBearingEnabled = true
                 }
                 mapViewportState.transitionToFollowPuckState()
-
-                // Set the map's min zoom
-                val cameraBoundsOptions = CameraBoundsOptions
-                    .Builder()
-                    .minZoom(7.0)
-                    .build()
-
-                mapView.mapboxMap.setBounds(cameraBoundsOptions)
-
-                // Get the places from the API
-                mapViewModel.getPlaces()
-            }
-
-            // Do something when the camera changes
-            MapEffect(cameraChanged) { mapView ->
-                val cameraBounds = mapView
-                    .mapboxMap
-                    .coordinateBoundsForCamera(mapView.mapboxMap.cameraState.toCameraOptions())
-
-                mapViewModel.setCurrentPointsList(cameraBounds)
-            }
-
-            // Draw the points on the map
-            currentPointList.forEach { place ->
-                CircleAnnotation(
-                    point = Point.fromLngLat(
-                        /* longitude = */ place.geometry.coordinates[0],
-                        /* latitude = */ place.geometry.coordinates[1]
-                    )
-                ) {
-                    circleRadius = 8.0
-                    circleColor = Color(0xffee4e8b)
-                    circleStrokeWidth = 2.0
-                    circleStrokeColor = Color(0xffffffff)
-                    interactionsState.onClicked {
-                        Toast.makeText(context, place.properties.name, Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                }
             }
         }
     }
 }
+
+
+//val coroutineScope = rememberCoroutineScope()
+//var cameraChanged by rememberSaveable { mutableStateOf(false) }
+//val mapState = rememberMapState {
+//    coroutineScope.launch {
+//        cameraChangedEvents.collect { _ ->
+//            cameraChanged = !cameraChanged
+//        }
+//    }
+//}
+
+// Set the map's min zoom
+//val cameraBoundsOptions = CameraBoundsOptions
+//    .Builder()
+//    .minZoom(7.0)
+//    .build()
+//
+//mapView.mapboxMap.setBounds(cameraBoundsOptions)
+
+// Do something when the camera changes
+//MapEffect(cameraChanged) { mapView ->
+//    val cameraBounds = mapView
+//        .mapboxMap
+//        .coordinateBoundsForCamera(mapView.mapboxMap.cameraState.toCameraOptions())
+//
+//    mapViewModel.setCurrentPointsList(cameraBounds)
+//}
+
+// Draw the points on the map
+//val currentPointList = mapViewModel.currentPointList
+//currentPointList.forEach { place ->
+//    CircleAnnotation(
+//        point = Point.fromLngLat(
+//            /* longitude = */ place.geometry.coordinates[0],
+//            /* latitude = */ place.geometry.coordinates[1]
+//        )
+//    ) {
+//        circleRadius = 8.0
+//        circleColor = Color(0xffee4e8b)
+//        circleStrokeWidth = 2.0
+//        circleStrokeColor = Color(0xffffffff)
+//        interactionsState.onClicked {
+//            Toast.makeText(context, place.properties.name, Toast.LENGTH_SHORT).show()
+//            true
+//        }
+//    }
+//}
--- app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapViewModel.kt
+++ app/src/main/java/com/alexisarevalor/decimetrixmap/feature/map/MapViewModel.kt
@@ -1,47 +1,51 @@
 package com.alexisarevalor.decimetrixmap.feature.map
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.alexisarevalor.decimetrixmap.core.network.NetworkConnectivityObserver
 import com.alexisarevalor.decimetrixmap.feature.map.data.Feature
 import com.alexisarevalor.decimetrixmap.feature.map.data.PlacesService
-import com.mapbox.geojson.Point
-import com.mapbox.maps.CoordinateBounds
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MapViewModel @Inject constructor(
-    private val placesService: PlacesService
+    private val placesService: PlacesService,
+    connectivityObserver: NetworkConnectivityObserver
 ) : ViewModel() {
 
     private val placesList = mutableListOf<Feature>()
+
+    val networkStatus = connectivityObserver.networkStatus
+        .stateIn(viewModelScope, SharingStarted.Eagerly, false)
 
     fun getPlaces() {
         viewModelScope.launch(Dispatchers.IO) {
             val placesResponse = placesService.getPlaces()
             if (placesResponse.isSuccessful && placesResponse.body() != null) {
+                placesList.clear()
                 placesList.addAll(placesResponse.body()!!.features)
             }
         }
     }
 
-
-    private val _currentPointsList = mutableStateListOf<Feature>()
-    val currentPointList: List<Feature> = _currentPointsList
-
-    fun setCurrentPointsList(cameraBounds: CoordinateBounds) {
-        val points = placesList.filter { place ->
-            val longitude = place.geometry.coordinates[0]
-            val latitude = place.geometry.coordinates[1]
-            val point = Point.fromLngLat(longitude, latitude)
-            cameraBounds.contains(point, true)
-        }
-
-        _currentPointsList.clear()
-        _currentPointsList.addAll(points)
-    }
-
 }
+
+//    private val _currentPointsList = mutableStateListOf<Feature>()
+//    val currentPointList: List<Feature> = _currentPointsList
+//
+//    fun setCurrentPointsList(cameraBounds: CoordinateBounds) {
+//        val points = placesList.filter { place ->
+//            val longitude = place.geometry.coordinates[0]
+//            val latitude = place.geometry.coordinates[1]
+//            val point = Point.fromLngLat(longitude, latitude)
+//            cameraBounds.contains(point, true)
+//        }
+//
+//        _currentPointsList.clear()
+//        _currentPointsList.addAll(points)
+//    }
